1a20d8fc039295314979d4d62da21343ea0d3453
Sia819 <jjn970819@gmail.com>
2020-02-20 14:36:55 +0900

    first upload

---
+++ TestProject_WPF/TestProject_WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Input;
+using System.Threading;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using ObservableExtension;  // my github extinsion https://github.com/Sia819/ObservableList
+
+namespace TestProject_WPF.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private ObservableList<Type> winList;
+        public ObservableList<Type> WinList
+        {
+            get { return winList; }
+            set { Set(ref winList, value, true); }
+        }
+
+        private int winList_SelectedIndex;
+        public int WinList_SelectedIndex
+        {
+            get { return winList_SelectedIndex; }
+            set { Set(ref winList_SelectedIndex, value); }
+        }
+
+        public ICommand GetStart { get; private set; }
+
+        [Obsolete]
+        public MainWindowViewModel()
+        {
+            WinList = new ObservableList<Type>();
+            GetStart = new RelayCommand(SelectedWindowStart);
+
+            var assems = Assembly.GetExecutingAssembly().DefinedTypes;
+            var result = from a in assems where a.BaseType.Name == "Window" orderby a.Name select a;
+            WinList.AddRange(result);
+        }
+
+        [STAThread]
+        [Obsolete]
+        private void SelectedWindowStart()
+        {
+            new Thread(() =>
+            {
+                Type listdata;
+                int index = WinList_SelectedIndex;  // user selected list of window / 사용자가 선택한 윈도우
+                if (index < 0) index = 0;
+                listdata = WinList[index];
+                var windowClass = Activator.CreateInstance(listdata); // This type can be create Instance of Window / 타입으로 윈도우 인스턴스를 만듦니다.
+                Window targetWindow = windowClass as Window;
+                targetWindow.ShowDialog();
+            })
+            {
+                ApartmentState = ApartmentState.STA,
+                IsBackground = true,
+            }.Start();
+        }
+    }
+}
---
+++ TestProject_WPF/TestProject_WPF/Views/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace TestProject_WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //this.DataContext = new ViewModels.MainWindowViewModel();
+        }
+    }
+}
